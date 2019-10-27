a2143f06ed4b942241a78077b536b38129c91c8a
BlueDespero <cezarytroska@gmail.com>
2019-10-27 19:59:07 +0100

    Update

--- l2z3.c
+++ l2z3.c
@@ -5,22 +5,21 @@
 char alphabet[128] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ,";
 int map[128];
 int map_off[128];
+int map_off_reverse[128];
 char code_table[64][64];
 char mode[256];
-char key[256];
+char key_text[256];
+int key_num[256];
 char text[256];
+int text_num[256];
 char output[256];
 
 void encode(void)
 {
-    for(unsigned int i = 0; i < strlen(key);i++)
+    for(unsigned long i = 0; i < strlen(text)-1;i++)
     {
-        int y1 = key[i];
-        int y2 = map[y1];
-        int y = map_off[y2];
-
-        int x1 = text[i];
-        int x = map[x1];
+        int y = key_num[i];
+        int x = map_off_reverse[text_num[i]];
         output[i] = code_table[x][y];
     }
 }
@@ -28,27 +27,22 @@ void encode(void)
 void decode(void)
 {
     int reverse_key[256];
-    for (unsigned int i = 0; i < strlen(key); i++)
+    for (unsigned int i = 0; i < strlen(text)-1; i++)
     {
-        int k1 = key[i];
-        int k2 = map[k1];
-        int k3 = map_off[k2];
-        reverse_key[i] = alphabet[(64 - k3)%64];
+        int k = key_num[i];
+        reverse_key[i] = (64 - k)%64;
     }
-    for(unsigned int i = 0; i < strlen(key);i++)
+    for(unsigned int i = 0; i < strlen(text)-1;i++)
     {
-        int y1 = reverse_key[i];
-        int y2 = map[y1];
-        //int y = map_off[y2];
-
-        int x1 = text[i];
-        int x = map[x1];
-        output[i] = code_table[x][y2];
+        int y = reverse_key[i];
+        int x = map_off_reverse[text_num[i]];
+        output[i] = code_table[x][y];
     }
 }
 
 int main(void)
 {
+    //mapping from letters to idx in normal alphabet
     for (int i = 0; i < 64; i++)
     {
         int h = alphabet[i];
@@ -62,26 +56,37 @@ int main(void)
         int j;
         scanf("%d",&j);
         map_off[i] = j;
+        map_off_reverse[j] = i;
         for (int x = 0; x < 64; x++)
-            code_table[i][x] = alphabet[(j+x)%64];
+        {
+            code_table[i][x] = alphabet[(j+x)%64];   
+        }
     }
 
-    fgets(key,256,stdin);
+    fgets(key_text,256,stdin);
     fgets(text,256,stdin);
     
-    int mod = strlen(key)-1;
-    for(unsigned int i = strlen(key)-1;i<strlen(text)-1;i++)
-        key[i] = key[i%mod];
-    
-    encode();
-    printf("\n%s",output);
+    //fixing the key length
+    int mod = strlen(key_text)-1;
+    for(unsigned long int i = 0;i<strlen(text)-1;i++)
+    {
+        int s1 = key_text[i%mod];
+        int s2 = map[s1];
+        key_num[i] = map_off[s2];        
+    }
 
-    /*
-    printf("Mode  %s",mode);
-    printf("Permutated  %s",permutated_alphabet);
-    printf("\nKey  %s",key);
-    printf("\nText  %s",text);
-    */
+    for(unsigned long int i = 0;i<strlen(text)-1;i++)
+    {
+        int s = text[i];
+        text_num[i] = map[s];        
+    }
+
+    if (mode[0] == 'd')
+        decode();
+    else
+        encode();
+    
+    printf("%s",output);
 
     return 0;
 }
